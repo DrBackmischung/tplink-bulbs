cb0410c8412f0d73b824beedcc9ac0fad1d8eb7b
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-06-10 11:38:52 +0200

    Switch tests to Jest

---
+++ test/color-helper.test.ts
@@ -0,0 +1,22 @@
+import { hexToHsl, rgbToHsl, getColor, temperature } from '../src/color-helper';
+
+describe('color-helper', () => {
+  test('throws on invalid input', () => {
+    expect(() => hexToHsl('#000000')).toThrow('Cannot set light to black');
+    expect(() => hexToHsl('#zzzzzz')).toThrow('Invalid hex string');
+    expect(() => temperature('2000K')).toThrow('Colour temperature should be between 2500K and 6500K.');
+    expect(() => getColor('invalid')).toThrow('Invalid Colour');
+    expect(() => getColor({ r: 0, g: 0, b: 0 })).toThrow('Cannot set light to black');
+  });
+
+  test('converts and parses colours', () => {
+    const redHsl = { hue: 0, saturation: 100, brightness: 50 };
+    expect(hexToHsl('#ff0000')).toEqual(redHsl);
+    expect(rgbToHsl({ r: 255, g: 0, b: 0 })).toEqual(redHsl);
+    expect(getColor('#ff0000')).toEqual(redHsl);
+    expect(getColor({ r: 255, g: 0, b: 0 })).toEqual(redHsl);
+    expect(getColor('red')).toEqual({ hue: 0, saturation: 100, color_temp: 0 });
+    expect(getColor('mint')).toEqual({ hue: 150, saturation: 100, color_temp: 0 });
+    expect(getColor('4500K')).toEqual({ color_temp: 4500 });
+  });
+});
